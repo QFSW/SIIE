c00b7272ab4ddd3588ac73c33486d094613fe81d
Yusuf Ismail <yusuf@qfsw.co.uk>
2020-05-05 18:45:57 +0100

    Restructured and added asmdefs

---
+++ Source/Editor/SelectableInversionInspector.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace QFSW.SIIE.Editor
+{
+    /// <summary>
+    /// Custom inspector for the SelectableInversion effect.
+    /// </summary>
+    [CustomEditor(typeof(SelectableInversion))]
+    public class SelectableInversionInspector : UnityEditor.Editor
+    {
+        private SerializedProperty useColoredInversion;
+        private SerializedProperty useMaskColor;
+        private SerializedProperty midInversionColor;
+        private SerializedProperty clearColor;
+
+        private void OnEnable()
+        {
+            useColoredInversion = serializedObject.FindProperty("useColoredInversion");
+            useMaskColor = serializedObject.FindProperty("useMaskColor");
+            midInversionColor = serializedObject.FindProperty("midInversionColor");
+            clearColor = serializedObject.FindProperty("clearColor");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(useColoredInversion, new GUIContent("Use Colored Inversion", "If the inverted image should converge to a colour as the inversion approaches 50%."));
+            if (useColoredInversion.boolValue)
+            {
+                EditorGUILayout.PropertyField(useMaskColor, new GUIContent("Use Mask Color", "Uses the color of the inversion camera's render texture as inversion converges to 50%."));
+                if (!useMaskColor.boolValue)
+                {
+                    EditorGUILayout.PropertyField(midInversionColor, new GUIContent("Mid Inversion Color", "The colour to converge to as the inversion converges to 50%."));
+                }
+            }
+
+            EditorGUILayout.PropertyField(clearColor, new GUIContent("Clear Color", "The background color that the image effect clears to."));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- Source/SelectableInversion.cs
+++ Source/SelectableInversion.cs
@@ -1,53 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-#if UNITY_EDITOR
-using UnityEditor;
-namespace QFSW.SIIE.Internal.Editor
-{
-    /// <summary>Custom inspector for the SelectableInversion effect.</summary>
-    [CustomEditor(typeof(SelectableInversion))]
-    public class SelectableInversionEditor : UnityEditor.Editor
-    {
-        private SerializedProperty useColoredInversion;
-        private SerializedProperty useMaskColor;
-        private SerializedProperty midInversionColor;
-        private SerializedProperty clearColor;
-
-        private void OnEnable()
-        {
-            useColoredInversion = serializedObject.FindProperty("useColoredInversion");
-            useMaskColor = serializedObject.FindProperty("useMaskColor");
-            midInversionColor = serializedObject.FindProperty("midInversionColor");
-            clearColor = serializedObject.FindProperty("clearColor");
-        }
-
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
-
-            EditorGUILayout.PropertyField(useColoredInversion, new GUIContent("Use Colored Inversion", "If the inverted image should converge to a colour as the inversion approaches 50%."));
-            if (useColoredInversion.boolValue)
-            {
-                EditorGUILayout.PropertyField(useMaskColor, new GUIContent("Use Mask Color", "Uses the color of the inversion camera's render texture as inversion converges to 50%."));
-                if (!useMaskColor.boolValue)
-                {
-                    EditorGUILayout.PropertyField(midInversionColor, new GUIContent("Mid Inversion Color", "The colour to converge to as the inversion converges to 50%."));
-                }
-            }
-
-            EditorGUILayout.PropertyField(clearColor, new GUIContent("Clear Color", "The background color that the image effect clears to."));
-
-            serializedObject.ApplyModifiedProperties();
-        }
-    }
-}
-#endif
+﻿using UnityEngine;
 
 namespace QFSW.SIIE
 {
-    /// <summary>Image effect for selectable inversion of the camera.</summary>
+    /// <summary>
+    /// Image effect for selectable inversion of the camera.
+    /// </summary>
     [ExecuteInEditMode]
     [RequireComponent(typeof(Camera))]
     [AddComponentMenu("Image Effects/Selectable Inversion")]
